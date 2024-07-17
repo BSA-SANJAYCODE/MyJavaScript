e2e0fa4cec6f69ee3e388461eb755d4057b465c2
BSA-SANJAYCODE <sanjay1978chauhan@gmail.com>
2024-07-17 17:13:00 +0000

    datatypes

--- JavaScriptBasic/datatypes.js
+++ JavaScriptBasic/datatypes.js
@@ -1,3 +1,18 @@
 "use strict"; // treat all js code as newer version
-alert("hello"); // we are using nodejs, not browser
+//alert("hello"); // we are using nodejs, not browser
 //two important documents tc39.es and mdn
+//data type
+/*
+number
+bigint
+string=>""
+boolean=>true/false
+null=>standalone value
+undefined
+symbol=> unique
+
+object
+
+*/
+console.log(typeof 3);
+console.log(typeof(undefined));
