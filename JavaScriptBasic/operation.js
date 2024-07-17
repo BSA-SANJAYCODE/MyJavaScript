da90a7028d5299345d3602ce63ca87bf5071a79b
BSA-SANJAYCODE <sanjay1978chauhan@gmail.com>
2024-07-17 18:05:54 +0000

    operations

---
+++ JavaScriptBasic/operation.js
@@ -0,0 +1,13 @@
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2%3);
+// console.log("1"+2+3);
+// console.log("1"+2+"3");
+// console.log(1+2+"3");
+// console.log(1+"2"+"3");
+let a=1;
+// b=++a+ ++a + ++a + ++a;
+b=a++ + a++ + a++ + a++;
+console.log((`a:${a}, b:${b}`));
