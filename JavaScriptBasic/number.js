039d416141cbec8755c3b3c99aadfbc082782c63
BSA-SANJAYCODE <sanjay1978chauhan@gmail.com>
2024-07-24 17:54:23 +0000

    math or num

---
+++ JavaScriptBasic/number.js
@@ -0,0 +1,17 @@
+const no=123;
+// console.log(no);
+const num=new Number(12.8956);
+//console.log(num);
+// console.log(num.toString().replace("1","2"));
+// console.log(num.toFixed(1));
+//precession means how many digits it take
+// console.log(num.toPrecision(3))
+// ====================================Math============================
+// const x=1234.564586;
+// console.log(Math.round(x));
+// console.log(Math.floor(x));
+// console.log(Math.ceil(x));
+console.log(Math.random());
+console.log(Math.floor((Math.random())+10)+1);
+console.log(Math.floor(Math.random()));
+
