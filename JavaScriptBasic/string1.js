57c18653d91f52178142d2f7ea841b667fa36d0f
BSA-SANJAYCODE <sanjay1978chauhan@gmail.com>
2024-07-24 00:08:02 +0000

    string1

--- JavaScriptBasic/string1.js
+++ JavaScriptBasic/string1.js
@@ -1,1 +1,17 @@
-console.log("Hello Sanjay")
+// console.log("Hello Sanjay")
+const s1="hello"
+const s2="bsa"
+s3=s1+s2
+// console.log(s3);
+// console.log(`My name is ${s1.toUpperCase()} and my second name is ${s2}`);
+const s=new String("ABCD Mukesh");
+// console.log(s.__proto__);
+// console.log(s.charAt(3));
+// console.log(s.indexOf('D'));
+const greeting = '   Hello world!             ';
+
+console.log(greeting);
+// Expected output: "   Hello world!   ";
+
+console.log(greeting.trim());
+// Expected output: "Hello world!";
