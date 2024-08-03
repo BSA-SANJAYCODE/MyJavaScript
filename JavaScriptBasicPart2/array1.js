42e1fe1e1da399969cd89a661bf3e3ed692affe7
BSA-SANJAYCODE <sanjay1978chauhan@gmail.com>
2024-08-03 08:44:03 +0000

    java script

--- JavaScriptBasicPart2/array1.js
+++ JavaScriptBasicPart2/array1.js
@@ -1,5 +1,34 @@
-const s1=new Array(1,2,3,4,5,6);
-s1.push(7,8,9)
-console.log(s1)
-s1.pop();
-console.log((s1));
+const s1=[1,2,3,4,5,6]
+const s2=[6,7,8,9]
+let a=10;
+let b=20;
+let c=30;
+// s1.unshift(10);
+// s1.shift();
+// const s2=s1.join();
+// console.log((typeof(s2)));
+// console.log("S1=", s1);
+// console.log("after using slice(1,3)",s1.slice(1,3));
+// console.log("S1=", s1);
+// console.log("after using splice(1,3)",s1.splice(1,3));
+// console.log("S1=", s1);
+// console.log(s1.join(s2));
+// const s3=s1.push(s2);
+// console.log(s3);
+// s1.push(s2)
+// console.log(s1);
+// console.log(s1[6][2]);
+// const s3=[...s1,...s2]
+// console.log(s3);
+console.log(Array.isArray("SANJAY"));
+console.log(Array.from("SANJAY"));
+console.log(Array.of(a,b,c))
+
+
+
+
+
+
+
+
+
