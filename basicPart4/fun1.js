0bb7834a94457cf62cc118e372d0a3ab5669f104
BSA-SANJAYCODE <sanjay1978chauhan@gmail.com>
2024-08-04 04:20:13 +0000

    javascript

---
+++ basicPart4/fun1.js
@@ -0,0 +1,50 @@
+// function login(name)
+// {
+//     if(!name)
+//     {
+//         console.log("please enter your name");
+        
+//     }
+//     else
+//     {
+//         return(`${name} logged In`)
+//     }
+
+// }
+// console.log(login("sanjay")
+// how to use rest operator ...
+// function calculateCartPrice(...num1)
+// {
+//     return num1
+// }
+// x=calculateCartPrice(50,40,60,70)
+// console.log(x);
+// const user=
+// {
+// username:"Sanjay",
+// prices:200
+// }
+
+// function handleObject(anyobject)
+// {
+//     console.log(`username is ${anyobject.username} and price is ${anyobject.price}`);
+
+// }
+// // handleObject(user)
+
+// handleObject({
+//     username:"sam",
+//     price:208,
+// })
+
+const list=[100,200,300,400];
+function returnvalue(anyvalue)
+{
+    return(anyvalue[3])
+}
+
+// console.log(returnvalue(list));
+x=returnvalue([500,"ram",23.56,"hello"]);
+console.log(x);
+
+
