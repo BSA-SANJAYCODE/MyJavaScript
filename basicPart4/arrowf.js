d5967049b55f772ab4da247acea8fe451175ec43
BSA-SANJAYCODE <sanjay1978chauhan@gmail.com>
2024-08-04 18:16:31 +0000

    function

---
+++ basicPart4/arrowf.js
@@ -0,0 +1,38 @@
+// function chai()
+// {
+//     console.log(this);
+    
+// }
+// chai()
+// const chai= function()
+// {
+//     let username="Monu";
+//     console.log(this.username);
+    
+// }
+// chai()
+// const chai=()=>
+// {
+//     let username="Monu";
+//     console.log(this.username);
+    
+// }
+// chai()
+// const chai=()=>
+// {
+//     let username="Monu";
+//     console.log(this.username);
+    
+// }
+// chai()
+
+// const addTwo=(num1,num2)=>{
+//     console.log(num1+num2);
+    
+// }
+// addTwo(5,6)
+// const addTwo=(num1,num2)=>(console.log(num1+num2));
+
+// addTwo(5,6)
+(()=>{console.log("hello")})();
+(()=>{console.log("sollo")})()
