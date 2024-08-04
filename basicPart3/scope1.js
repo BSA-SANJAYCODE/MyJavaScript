b3a6b3cb85bcd6e7467ff488c7337cbc0b42cc8d
BSA-SANJAYCODE <sanjay1978chauhan@gmail.com>
2024-08-04 08:34:17 +0000

    javascript

---
+++ basicPart3/scope1.js
@@ -0,0 +1,47 @@
+
+// function one()
+// {
+//     username="Monu";
+//     function two()
+//     {
+//         age=30
+//         console.log("my name is=",username);
+              
+//     }
+//      two()
+//      console.log("My age is=",age); 
+// }
+// one()
+// if(true)
+// {
+//     username="monu"
+//     if(username==="monu")
+//     {
+//         age=30
+//         // console.log("My name is=",username);
+//     }
+//     console.log("My age is=",age);
+// }
+// console.log(username);
+// -------------------------------
+// Output
+// My age is=30
+// monu
+
+const addOne=function(num1)
+{
+    console.log(num1+5);
+    
+}
+addOne(2)
+addTwo(3)
+function addTwo(num1)
+{
+    console.log(num1+10);
+    
+}
+
+
+
+
+
