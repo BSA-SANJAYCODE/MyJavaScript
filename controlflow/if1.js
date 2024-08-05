67de7cce94830ceab64e26680224e7f2e9a0ca47
BSA-SANJAYCODE <sanjay1978chauhan@gmail.com>
2024-08-05 17:44:13 +0000

    python

---
+++ controlflow/if1.js
@@ -0,0 +1,11 @@
+// x=2;
+// if(x==="2")
+// {
+//     console.log("x is equal to 2");
+//     console.log(typeof(x));
+//     console.log(typeof("2"));
+    
+// }
+var1=null??" abc "??20;
+console.log(var1);
+
