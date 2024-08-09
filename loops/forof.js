c935e1b44c5372d62e1163d593c82fbba6f82dc5
BSA-SANJAYCODE <sanjay1978chauhan@gmail.com>
2024-08-09 16:51:14 +0000

    loops

---
+++ loops/forof.js
@@ -0,0 +1,11 @@
+const letter="India";
+for (const element of letter) 
+    {
+     if(element==='I')
+     {
+        console.log(`Capital letter in india= ${element}`);
+        break
+     }
+     console.log(element);
+     
+}
